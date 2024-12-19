2dbe0442eb2b71bed7cd2832c1ec2f8feb0e1208
Teja <awstheja727@gmail.com>
2024-12-19 10:05:10 +0530

    Catalogue script

---
+++ catalogue.sh
@@ -0,0 +1,19 @@
+cp catalogue.service /etc/systemd/system/catalogue.service
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+useradd roboshop
+mkdir /app
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+cd /app
+unzip /tmp/catalogue.zip
+cd /app
+npm install
+dnf install mongodb-mongosh -y
+mongosh --host mongodb.tejadevopsb81.icu </app/db/master-data.js
+
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl restart catalogue
