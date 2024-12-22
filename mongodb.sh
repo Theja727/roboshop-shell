ce5db719bb08640c7c1387fb81b03a6b09d6c6f7
Teja <awstheja727@gmail.com>
2024-12-22 21:40:21 +0530

    Shell script changes for common.sh and MongoDB

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,19 @@
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+source common.sh
+app_name=mongodb
 
-dnf install mongodb-org -y
+print_heading "Copy MongoDB repo file"
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>$log_file
+status_check $?
 
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-systemctl enable mongod
-systemctl restart mongod
+print_heading "Install MongoDB Server"
+dnf install mongodb-org -y &>>$log_file
+status_check $?
+
+print_heading "Update MongoDB listen address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>$log_file
+status_check $?
+
+print_heading "Start MongoDB service"
+systemctl enable mongod &>>$log_file
+systemctl restart mongod &>>$log_file
+status_check $?
